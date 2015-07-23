b1a113b19ef9875407f78ffdca10a1736e642ca4
lynx <lynxsjob@sina.com>
2015-07-23 16:06:35 +0800

    15 th commit

---
+++ WebRoot/js/sys/Test.java
@@ -0,0 +1,16 @@
+package cn.lynx.core;
+public class hello{
+   int a;
+   public void static main(String args[]){
+     a=241;
+   }
+   public void test1(){
+     int a=2;
+   }
+   public void test2(){
+	int a=3;
+    }
+    public void test3(){
+    	int a=244;
+    }
+}
